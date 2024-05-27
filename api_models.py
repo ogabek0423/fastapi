4d6fb25bd96deeeb7ff268aab062fb3c3b3a7e7d
ogabek0423 <ogabek0423@gmail.com>
2024-05-27 10:58:31 +0500

    news

--- api_models.py
+++ api_models.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+
 class Item(BaseModel):
     name: str
     description: str | None = None
@@ -11,9 +12,33 @@ class Item(BaseModel):
     tax: float | None = None
 
 
+@app.get("/")
+async def read_root():
+    return {"message": "1-page"}
+
+
 @app.get('/items')
 async def read_items() -> list[Item]:
     return [
-        Item(name='test name', description='test description', price=123),
-        Item(name='test name', description='test description', price=321),
+        Item(name='test name 1', description='test description', price=123, tags=['tag1']),
+        Item(name='test name 2', description='test description', price=321, tags=['tag2']),
+        Item(name='test name 3', description='test description', price=456, tags=['tag3']),
+        Item(name='test name 4', description='test description', price=678, tags=['tag4']),
+        Item(name='test name 5', description='test description', price=789, tags=['tag5']),
     ]
+
+
+@app.post('/item_post')
+async def create_item(item: Item) -> Item:
+    return item
+
+
+'''example for post 
+{
+    "name": "test",
+    "description": "atest",
+    "price": 100.25,
+    "tags": [],
+    "tax": null
+}
+'''
