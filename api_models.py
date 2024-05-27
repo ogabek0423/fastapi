f986a83ace097939db2470dd17b7c28ef9f56e4e
ogabek0423 <ogabek0423@gmail.com>
2024-05-27 11:17:42 +0500

    news

--- api_models.py
+++ api_models.py
@@ -20,10 +20,15 @@ async def read_root():
 @app.get('/items')
 async def read_items() -> list[Item]:
     return [
+        # misol1
         Item(name='test name 1', description='test description', price=123, tags=['tag1']),
+        # misol2
         Item(name='test name 2', description='test description', price=321, tags=['tag2']),
+        # misol3
         Item(name='test name 3', description='test description', price=456, tags=['tag3']),
+        # misol4
         Item(name='test name 4', description='test description', price=678, tags=['tag4']),
+        # misol5
         Item(name='test name 5', description='test description', price=789, tags=['tag5']),
     ]
 
@@ -33,11 +38,40 @@ async def create_item(item: Item) -> Item:
     return item
 
 
-'''example for post 
+'''example for post  insomnia
 {
     "name": "test",
     "description": "atest",
     "price": 100.25,
+    "tags": [],
+    "tax": null
+}
+
+{
+    "name": "test2",
+    "description": "testdeescription",
+    "price": 34.25,
+    "tags": [],
+    "tax": null
+}
+{
+    "name": "fast",
+    "description": "apitest",
+    "price": 10.10,
+    "tags": [],
+    "tax": null
+}
+{
+    "name": "test4",
+    "description": "fastapidescriptions",
+    "price": 10.25,
+    "tags": [],
+    "tax": null
+}
+{
+    "name": "finally",
+    "description": "description5",
+    "price": 20.20,
     "tags": [],
     "tax": null
 }
